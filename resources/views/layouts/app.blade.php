5f15256fc773dd12ad0234fe515d0370c18364a9
Szymon Gałąska <siubidubasiabadabba@gmail.com>
2025-01-13 00:05:12 +0100

    created sidenav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,25 +10,26 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900 flex">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            @isset($header)
+            <!-- @isset($header)
                 <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endisset
+            @endisset -->
 
             <!-- Page Content -->
-            <main>
+            <main class="w-full mx-6 sm:ml-24 sm:mr-0">
                 {{ $slot }}
             </main>
         </div>
