738e233834b0d88423f3ebe5a7b2ca2a533f3625
Szymon Gałąska <siubidubasiabadabba@gmail.com>
2025-01-13 14:07:24 +0100

    Created Weather model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable implements MustVerifyEmail
             'password' => 'hashed',
         ];
     }
+
+    public function weather()
+    {
+        return $this->HasMany(\App\Models\Weather::class);
+    }
 }
